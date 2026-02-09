b9ffb6ce2ac78f78492b858eb7959562eb39820a
Yassin Shaheen <yassinxs22@gmail.com>
2026-02-08 21:15:37 -0500

    Add footer

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import { socialImgs } from "../constants";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="footer">
+      <div className="footer-container">
+        <div className="flex flex-col justify-center">
+          <p>Blog</p>
+        </div>
 
-export default Footer
+        <div className="socials">
+          {socialImgs.map((socialImg, index) => (
+            <a
+              key={index}
+              href={socialImg.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="icon"
+              aria-label={socialImg.name}
+            >
+              <img
+                src={socialImg.imgPath}
+                alt={socialImg.name}
+              />
+            </a>
+          ))}
+        </div>
+
+        <div className="flex flex-col justify-center">
+          <p className="text-center md:text-end">
+            © {new Date().getFullYear()} Yassin Shaheen. All rights reserved.
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
