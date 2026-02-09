6e4e078928c8e61718612559f0ca3b7201cf114f
Yassin Shaheen <yassinxs22@gmail.com>
2026-02-08 21:51:44 -0500

    Fixed capitilization

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -2,7 +2,7 @@ import { useRef, useState } from "react";
 import emailjs from "@emailjs/browser";
 
 import TitleHeader from "../components/TitleHeader";
-import ContactExperience from "../components/models/contact/ContactExperience";
+import ContactExperience from "../components/models/Contact/ContactExperience";
 
 const Contact = () => {
   const formRef = useRef(null);
